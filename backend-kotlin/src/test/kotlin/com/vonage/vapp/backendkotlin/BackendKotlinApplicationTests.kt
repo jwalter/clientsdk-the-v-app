2dbb56f7d85657e4672ef3d7f3c7e26ee39404da
Joakim Waltersson <Joakim.waltersson@gmail.com>
2020-11-09 21:28:54 +0100

    Project base from start.spring.io
    https://start.spring.io/#!type=gradle-project&language=kotlin&platformVersion=2.4.0.RC1&packaging=jar&jvmVersion=11&groupId=com.vonage.vapp&artifactId=backend-kotlin&name=backend-kotlin&description=Kotlin%20backend%20for%20The%20V-app&packageName=com.vonage.vapp.backend-kotlin&dependencies=webflux,devtools

---
+++ backend-kotlin/src/test/kotlin/com/vonage/vapp/backendkotlin/BackendKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vonage.vapp.backendkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
